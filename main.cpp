266a6667e4c0bb9bc800d66acdb0f531dcf71527
= <definitelynotkevincs@gmail.com>
2025-12-20 21:47:30 -0600

    just basic change

--- main.cpp
+++ main.cpp
@@ -8,13 +8,14 @@ int main(){
     vector<thread> workers;
     mutex log_mtx;
 
-    for(int i = 0; i < 4; ++i)
+    for(int i = 0; i < 4; ++i){
         workers.emplace_back([&jq]{
             while(true){
                 auto job = jq.pop();
                 job();
             }
-    });
+        });
+    }
 
     for(int i = 0; i < 100; ++i){
     jq.push([&log_mtx](){
